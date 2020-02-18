0e9b6da535f46091b06ad52c6a592e42d64b6239
Andrew Syriala <asyriala@overstock.com>
2020-02-17 22:27:19 -0700

    add enzyme, plop, and plop tempates

---
+++ plopfile.js
@@ -0,0 +1,155 @@
+const promptDirectory = require('inquirer-directory');
+
+module.exports = plop => {
+    function isComponent(type) {
+        return type === 'Functional Component'
+            || type === 'Classy Component'
+            || type === 'Functional Component (connected)'
+            || type === 'Classy Component (connected)';
+    }
+
+    plop.setPrompt('directory', promptDirectory);
+    plop.setGenerator('actions', {
+        description: 'Generate file types',
+        prompts: [
+            {
+                type: 'list',
+                name: 'fileType',
+                message: 'What shall we create?',
+                choices: [
+                    'actions',
+                    'entities',
+                    'Functional Component',
+                    'Functional Component (connected)',
+                    'Classy Component',
+                    'Classy Component (connected)',
+                    'viewStates'
+                ]
+            },
+            {
+                type: 'input',
+                name: 'componentName',
+                message: 'What shall we name this component?',
+                when: data => {
+                    return isComponent(data.fileType);
+                }
+            },
+            {
+                basePath: './src',
+                type: 'directory',
+                name: 'directory',
+                message: 'Where shall we put it?'
+            }
+        ],
+        actions: data => {
+            if (isComponent(data.fileType)) {
+                const actions = [];
+                switch (data.fileType) {
+                    case 'Classy Component': {
+                        actions.push({
+                            type: 'add',
+                            path: 'src/{{directory}}/{{componentName}}.js',
+                            templateFile: 'plop-templates/ClassyComponent.hbs'
+                        });
+
+                        actions.push({
+                            type: 'add',
+                            path: 'src/{{directory}}/{{componentName}}.test.js',
+                            templateFile: 'plop-templates/ComponentTest.hbs'
+                        });
+
+                        actions.push({
+                            type: 'add',
+                            path: 'src/{{directory}}/{{componentName}}.less',
+                            templateFile: 'plop-templates/ComponentLess.hbs'
+                        });
+
+                        break;
+                    }
+
+                    case 'Functional Component': {
+                        actions.push({
+                            type: 'add',
+                            path: 'src/{{directory}}/{{componentName}}.js',
+                            templateFile: 'plop-templates/FunctionalComponent.hbs'
+                        });
+
+                        actions.push({
+                            type: 'add',
+                            path: 'src/{{directory}}/{{componentName}}.test.js',
+                            templateFile: 'plop-templates/ComponentTest.hbs'
+                        });
+
+                        actions.push({
+                            type: 'add',
+                            path: 'src/{{directory}}/{{componentName}}.less',
+                            templateFile: 'plop-templates/ComponentLess.hbs'
+                        });
+                        break;
+                    }
+
+                    case 'Functional Component (connected)': {
+                        actions.push({
+                            type: 'add',
+                            path: 'src/{{directory}}/{{componentName}}.js',
+                            templateFile: 'plop-templates/FunctionalComponentConnected.hbs'
+                        });
+
+                        actions.push({
+                            type: 'add',
+                            path: 'src/{{directory}}/{{componentName}}.test.js',
+                            templateFile: 'plop-templates/ComponentTest.hbs'
+                        });
+
+                        actions.push({
+                            type: 'add',
+                            path: 'src/{{directory}}/{{componentName}}.less',
+                            templateFile: 'plop-templates/ComponentLess.hbs'
+                        });
+
+                        break;
+                    }
+
+                    case 'Classy Component (connected)': {
+                        actions.push({
+                            type: 'add',
+                            path: 'src/{{directory}}/{{componentName}}.js',
+                            templateFile: 'plop-templates/ClassyComponentConnected.hbs'
+                        });
+
+                        actions.push({
+                            type: 'add',
+                            path: 'src/{{directory}}/{{componentName}}.test.js',
+                            templateFile: 'plop-templates/ComponentTest.hbs'
+                        });
+
+                        actions.push({
+                            type: 'add',
+                            path: 'src/{{directory}}/{{componentName}}.less',
+                            templateFile: 'plop-templates/ComponentLess.hbs'
+                        });
+
+                        break;
+                    }
+
+                    default:
+                }
+
+                return actions;
+            }
+
+            return [
+                {
+                    type: 'add',
+                    path: 'src/{{directory}}/{{fileType}}.js',
+                    templateFile: 'plop-templates/{{fileType}}.hbs'
+                },
+                {
+                    type: 'add',
+                    path: 'src/{{directory}}/{{fileType}}.test.js',
+                    templateFile: 'plop-templates/{{fileType}}.test.hbs'
+                }
+            ];
+        }
+    });
+};
