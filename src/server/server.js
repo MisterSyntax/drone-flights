0393a6ef14b47d500f55594807fa43ca5d9a2afb
Andrew Syriala <asyriala@overstock.com>
2020-02-20 00:49:04 -0700

    add cors handler

--- src/server/server.js
+++ src/server/server.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const path = require('path');
-const routes = require('./routes/routes.js');
 const bodyParser = require('body-parser');
+
+const enableCORS = require('./enableCORS/enableCORS');
+const routes = require('./routes/routes.js');
 
 const server = express();
 
-server.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
+server.use(enableCORS);
 server.use(bodyParser.json());
 server.use(express.static(path.join(__dirname, 'build')));
 server.use(routes);
